9605769542e74a719993e3a8edb111163c964087
Jane Kim <jane.kim0717@gmail.com>
2022-04-04 11:40:43 -0400

    commit tree 1713000642c842cfbd3f70120f0a4b930c5d7736

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "Jane", last_name: "Kim", grade: 12)
+Student.create(first_name: "Steven", last_name: "McDaniel", grade: 11)
